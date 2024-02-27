a235b24b5ea7fcd45e1c95dcaaf3d3e87d4602cd
SpakBogdan <spakbogdan97@gmail.com>
2024-02-27 19:28:56 +0100

    update

---
+++ lesson_8/code/classwork/Taxes.java
@@ -0,0 +1,23 @@
+package classwork;
+/*
+Напишите программу, которая запрашивает у пользователя его доход и вычисляет сумму налога согласно прогрессивной шкале налогообложения (например, 10% на доход до 10 000, 20% на доход от 10 001 до 20 000 и т.д.).
+*/
+
+import java.util.Scanner;
+
+public class Taxes {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input you month income: ");
+        double income = scanner.nextInt();
+
+        if (income <= 10000) {
+            System.out.println("Taxes = " + income * 0.1);
+        } else if (income >= 10001 && income <= 20000) {
+            System.out.println("Taxes = " + income * 0.2);
+        } else {
+            System.out.println("Taxes = " + income * 0.3);
+        }
+    }
+}
